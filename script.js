0a6bbf033ec58444f11f1e57677845b63d30156f
Velonti <maciej1.gembiak@gmail.com>
2026-03-17 15:18:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('startBtn').addEventListener('click', function() {
+    alert("UWAGA: Skrypt został uruchomiony. Przeglądarka może przestać odpowiadać!");
+    
+    // To jest pętla, o którą pytałeś
+    while(true) {h3zVhr
+        console.log("Ładowanie danych...");
+        // Tutaj procesor zaczyna pracować na 100%
+    }
+});
